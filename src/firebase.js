669eae8ca663f02304220aa7d2c8a3071190aa78
IR~SAGE <sibo.egide98@gmail.com>
2021-02-05 15:22:25 +0200

    Setting Up Project Structure

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBeUV-AQQn-9m_DNCbg9580hGpA4S07lqQ",
+    authDomain: "whatsapp-clone-23dc1.firebaseapp.com",
+    projectId: "whatsapp-clone-23dc1",
+    storageBucket: "whatsapp-clone-23dc1.appspot.com",
+    messagingSenderId: "801860141343",
+    appId: "1:801860141343:web:38db276e6c7d0c7628f02a",
+    measurementId: "G-CNRMBJC30M"
+  };
