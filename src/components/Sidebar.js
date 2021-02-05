9bda1dd8d7906cfdd09e0cda45d52da88c642d80
IR~SAGE <sibo.egide98@gmail.com>
2021-02-05 22:09:28 +0200

    Retrieving Rooms From The DataBase

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
 
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
@@ -7,8 +7,22 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { Avatar, IconButton } from "@material-ui/core";
 import { SearchOutlined } from "@material-ui/icons";
 import SidebarChat from "./SidebarChat";
+import db from "../firebase";
 
 function Sidebar() {
+
+    const [rooms, setRooms] = useState([]);
+
+useEffect(() => {
+    db.collection('rooms').onSnapshot(snapshot => (
+        setRooms(snapshot.docs.map(doc => ({
+            id: doc.id,
+            data:doc.data()
+       })))
+   )) 
+   
+}, [])
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -35,7 +49,9 @@ function Sidebar() {
 
       <div className="sidebar__chats">
               <SidebarChat addNewChart/>
-              <SidebarChat />
+              {rooms.map(room => (
+                  <SidebarChat key={room.id} id={ room.id} name = {room.data.name} />
+              ))}
       </div>
     </div>
   );
--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,31 +1,30 @@
-import { Avatar } from '@material-ui/core';
-import React from 'react';
+import { Avatar } from "@material-ui/core";
+import React from "react";
 import "./SidebarChat.css";
+import avatar from "../avatar.png"
 
-function SidebarChat({ addNewChart }) {
-    
-    const createChat = () => {
-        const roomName = prompt("Please Enter Name For The Chat");
+function SidebarChat({ id, name, addNewChart }) {
+  const createChat = () => {
+    const roomName = prompt("Please Enter Name For The Chat");
 
-        if (roomName) {
-            // do this
-        }
-    };
+    if (roomName) {
+      // do this
+    }
+  };
 
-    return !addNewChart ? (
-        <div className="sidebarChat">
-            <Avatar />
-            <div className="sidebarChat__info">
-                <h2>Room Name</h2>
-                <p>This Is The Last Message</p>
-                
-            </div>
-        </div>
-    ) : (
-            <div onClick={createChat} className="sidebarChat">
-                <h2>Add New Chat</h2>
-       </div>     
-    );
+  return !addNewChart ? (
+    <div className="sidebarChat">
+          <Avatar src={ avatar} />
+      <div className="sidebarChat__info">
+        <h2>{name}</h2>
+        <p>This Is The Last Message</p>
+      </div>
+    </div>
+  ) : (
+    <div onClick={createChat} className="sidebarChat">
+      <h2>Add New Chat</h2>
+    </div>
+  );
 }
 
-export default SidebarChat
+export default SidebarChat;
