fd6a92feb192e21d570679e16fb2cc78d622005a
IR~SAGE <sibo.egide98@gmail.com>
2021-02-06 06:50:02 +0200

    Adding Google Authentication

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-
-import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
-import './App.css';
-import Chat from './components/Chat';
-import Sidebar from './components/Sidebar';
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Chat from "./components/Chat";
+import Login from "./components/Login";
+import Sidebar from "./components/Sidebar";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
   return (
     //BEM Maming Convestion
     <div className="app">
-     
-      <div className="app__body">
-        <Router>
-        <Sidebar />
-          <Switch>
 
-            <Route path="/rooms/:id">
-            <Chat />
-            </Route>
-            <Route path="/">
-              {/*<Chat />*/}
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+       <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:id">
+                <Chat />
+              </Route>
+              <Route path="/">{/*<Chat />*/}</Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,10 +8,12 @@ import { Avatar, IconButton } from "@material-ui/core";
 import { SearchOutlined } from "@material-ui/icons";
 import SidebarChat from "./SidebarChat";
 import db from "../firebase";
+import { useStateValue } from "../StateProvider";
 
 function Sidebar() {
 
-    const [rooms, setRooms] = useState([]);
+  const [rooms, setRooms] = useState([]);
+  const [{user}, dispatch] = useStateValue();
 
 useEffect(() => {
     const unsubscribe = db.collection('rooms').onSnapshot(snapshot => (
@@ -29,7 +31,7 @@ useEffect(() => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar src="" />
+        <Avatar src={user?.photoURL} />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
